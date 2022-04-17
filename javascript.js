6875ed4bb396a6e673ff300895494c55fd637b10
Mohammed Zia <mohammed-zia@outlook.com>
2022-04-17 09:35:38 +0100

    Adding calculator layout & buttons

--- javascript.js
+++ javascript.js
@@ -1,30 +1,64 @@
+const calcButton = document.getElementsByClassName("calcButton");
+
 const add = function(a,b) {
-return a + b
+return a + b;
 };
 
 const subtract = function(a,b) {
-	return a - b
+	return a - b;
 };
 
 const multiply = (a,b) => {
-    return a*b
+    return a*b;
 }
 
 
 const divide = (a,b) => {
-    return a/b
+    return a/b;
 }
 
+const operate = (op, a, b) => {
+    return op(a,b);
+}
 
-const multiplyArr = function(arr) {
-  let total = 0;
-  for(let i = 0; i<arr.length; i++){
-    if(i === 0){
-      total = arr[i]
+function genDivs(v){
+    const e = document.querySelector('.calculator'); 
+    var display = document.createElement('div');
+    display.className = "display";
+    display.style.width = "100%";
+    display.style.height = "100px";
+    display.textContent = "5318008";
+    e.appendChild(display)
+    for(var i = 0; i < v; i++){ 
+    var row = document.createElement("div"); 
+    row.className = "row"; 
+    for(var x = 1; x <= v; x++){ 
+        var cell = document.createElement("div"); 
+        cell.className = "calcButton";
+        cell.style.width = "100%";
+        let height = 700 / parseInt(v);
+        cell.style.height = `${height}px`
+        row.appendChild(cell); 
+    } 
+    e.appendChild(row); 
     }
-    else{
-      total *= arr[i]
+    for (let i = 0; i < calcButton.length; i++) {
+        calcButton[i].addEventListener("click", () => {
+        calcButton[i].className += " squareBlack";
+ 
+        });
+        calcButton[i].id = i;
     }
-  }
-  return total
-};
+    numOrder = ["7", "8", "9", "+", "4","5","6","-","1","2","3","*","0",".","=","/"]
+    for (let i = 15; i >= 0; i--){
+        calcButton[i].textContent = numOrder[i]    
+    }
+    const clearBtnDiv = document.createElement('div');
+    e.appendChild(clearBtnDiv)
+    clearBtnDiv.className = "clearButtonDiv"
+    const clearBtn = document.createElement('button');
+    clearBtn.textContent='Clear';
+    clearBtnDiv.appendChild(clearBtn);
+    clearBtn.className = "clearButton"
+
+}
